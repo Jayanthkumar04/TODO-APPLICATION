3fba8af8610e2a7843a54faed1fc663603cf42cd
jayanth <jayanth.yadavalli@ascendion.com>
2024-11-05 16:36:24 +0530

    TA-18:Added todo list tasks into the array and displayed it dynamically

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 "use strict";
 let inputField = document.querySelector("#input-box");
+let box = document.querySelector(".toDoList");
 let itemList = document.querySelector(".toDoList");
+let itemsArray = [];
 function onEnter(event) {
     if (event.key === "Enter" && inputField.value.trim() !== "") {
-        const listItem = document.createElement("li");
-        listItem.classList.add("box");
-        listItem.textContent = inputField.value;
-        itemList.appendChild(listItem);
+        itemsArray.unshift(inputField.value);
         inputField.value = "";
+        updateList();
+    }
+    function updateList() {
+        itemList.innerHTML = "";
+        itemsArray.forEach((item) => {
+            box.style.display = "block";
+            const listItem = document.createElement("li");
+            listItem.classList.add("box");
+            listItem.textContent = item;
+            itemList.append(listItem);
+            listItem.style.overflow = "auto";
+            listItem.style.fontSize = "1.5em";
+            inputField.value = "";
+        });
     }
 }
 inputField.addEventListener("keydown", onEnter);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 let inputField = document.querySelector("#input-box") as HTMLInputElement;
-
+let box = document.querySelector(".toDoList") as HTMLElement;
 let itemList = document.querySelector(".toDoList") as HTMLUListElement;
+let itemsArray: string[] = [];
+ 
 function onEnter(event:KeyboardEvent){
-
+ 
     if(event.key === "Enter" && inputField.value.trim() !== ""){
-
-        const listItem = document.createElement("li");
-        listItem.classList.add("box")
-        listItem.textContent = inputField.value;
-         
-        itemList.appendChild(listItem);
-
-        inputField.value="";
+        itemsArray.unshift(inputField.value);
+        inputField.value = "";
+        updateList();
+    }
+    function updateList(){
+        itemList.innerHTML="";
+        itemsArray.forEach((item) => {
+            box.style.display="block";
+            const listItem = document.createElement("li");
+            listItem.classList.add("box")
+            listItem.textContent = item;
+            itemList.append(listItem);
+            listItem.style.overflow="auto";
+            listItem.style.fontSize="1.5em";
+ 
+            inputField.value="";
+        });
     }
 }
 inputField.addEventListener("keydown",onEnter);
+ 
+ 
+ 
